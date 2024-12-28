4c59b9f802c0f90323ca041f4b2962059bf395a4
VaishooR <vaishooramkrish@gmail.com>
2024-12-28 14:31:47 +0530

    multiple request handlers, next(), order of execution

--- src/app.js
+++ src/app.js
@@ -2,12 +2,27 @@ const express = require("express"); //
 const app = express(); //
 
 
-app.get("/user",(req,res)=>res.send("Get data"));
-app.post("/user",(req,res)=>res.send("Post data"));
-app.put("/user",(req,res)=>res.send("Put data"));
-
-
-app.use("/user",(req,res)=> res.send("App use"));
+app.get("/user",(req,res,next)=>{
+    console.log('rh1')
+    next()
+    // res.send("res rh1")
+    
+})
+app.get("/user",(req,res,next)=>{
+    console.log('rh2')
+    // res.send("res rh2")
+    next()
+})
+app.get("/user",(req,res,next)=>{
+    console.log('rh3')
+    // res.send("res rh3")
+    next()
+})
+app.get("/user",(req,res,next)=>{
+    console.log('rh4')
+    res.send("res rh4")
+    // next()
+})
 
 
 app.listen(7777,()=>{
