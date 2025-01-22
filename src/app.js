a094daaea54331b0913b8681f8ee887d2c837807
VaishooR <vaishooramkrish@gmail.com>
2025-01-22 12:37:07 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const connectDB = require('./config/database');
 const cookieParser = require('cookie-parser');
-
+const cors = require("cors");
 const authRouter = require('./routes/auth');
 const profileRouter = require('./routes/profile');
 const requestRouter = require('./routes/request');
@@ -10,7 +10,10 @@ const requestRouter = require('./routes/request');
 // Middleware which converts data received from the client to json format
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials : true
+}));
 
 app.use('/',authRouter);
 app.use('/',profileRouter);
